882512560fed2e10653aa15566aa4a80569d09d7
anderson <anderson@35aeffee-e24b-0410-9652-c31049cf9950>
2007-05-16 21:49:22 +0000

    made server create client_proxy

--- src/game_server.rb
+++ src/game_server.rb
@@ -1,2 +1,11 @@
+require 'game_client_proxy'
 class GameServer
+  def initialize()
+    @game_client_proxy = GameClientProxy.new
+    @game_client_proxy.start
+  end
+end
+
+if $0 == __FILE__
+  GameServer.new
 end
