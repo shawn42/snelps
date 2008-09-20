ea0fc5d819cfc92684a1fbf1c194534a343c9935
shawn42 <shawn42@35aeffee-e24b-0410-9652-c31049cf9950>
2008-09-20 01:00:37 +0000

    put platform checking that I turned off for ruby 1.9 testing, back in

--- lib/platform.rb
+++ lib/platform.rb
@@ -1,15 +1,16 @@
 # Helper functions for determining platform
 
+# TODO how do I do this in ruby 1.9, since PLATFORM doesn't exist?
 class Platform
 	def self.mac?
-	  return true#PLATFORM =~ /darwin/
+	  return PLATFORM =~ /darwin/
 	end
 	
 	def self.windows?
-	  return false#PLATFORM =~ /mswin/
+	  return PLATFORM =~ /mswin/
 	end
 	
 	def self.linux?
-		return false#PLATFORM =~ /linux/
+		return PLATFORM =~ /linux/
 	end
 end
